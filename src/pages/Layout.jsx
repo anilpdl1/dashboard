fb696e8ee6121c98778fda89cae7e6b0568ea2c7
Anil <paudelanil714@gmail.com>
2026-09-04 21:16:03 +0545

    formatting

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Menu from '../components/Menu'
+const Layout = () => {
+  return (
+    <div className='min-h-screen'>
+      <Navbar/>
+      <Menu/>
+      <main className='pt-16'>
+        <Outlet/>
+      </main>
+    </div>
+  )
+}
+
+export default Layout
